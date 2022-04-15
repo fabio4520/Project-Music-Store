d0eccf952a422059c48d47300fc41bba6a290c14
fabio4520 <fabioleofc@gmail.com>
2022-04-15 12:54:41 -0500

    ADD: Order model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_15_175141) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_15_175337) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_15_175141) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.date "date"
+    t.float "total"
+    t.bigint "customers_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customers_id"], name: "index_orders_on_customers_id"
+  end
+
   create_table "songs", force: :cascade do |t|
     t.string "name"
     t.integer "duration"
@@ -52,5 +61,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_15_175141) do
   end
 
   add_foreign_key "albums", "artists"
+  add_foreign_key "orders", "customers", column: "customers_id"
   add_foreign_key "songs", "albums"
 end
